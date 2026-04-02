6d660a7d0b7fcd71061f5c836adb1cd6e32e019f
JashliXD <gabrielvillenamc@gmail.com>
2026-04-02 16:03:32 +0800

    Update cell

--- src/cell.cpp
+++ src/cell.cpp
@@ -1,7 +1,19 @@
 #include "cell.hpp"
 
-Cell::Cell(){
+Cell::Cell(int x, int y, int size){
+    this->position = Vector2{(float) x, (float) y};
+    this->size = size;
     this->isMine = false;
     this->isFlagged = false;
-    this->neighboor = 0;
+    this->neighbor = 0;
+    this->isSearched = false;
 }
+
+void Cell::draw(){
+    DrawRectangle(position.x, position.y, size, size, color[0]);
+}
+
+void Cell::drawTexture(){
+    DrawText(getImages(0));
+}
+
--- src/cell.hpp
+++ src/cell.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <raylib.h>
+#include "colors.hpp"
+#include "image.hpp"
 
 struct Cell{
     bool isMine;
     bool isFlagged;
-    int neighboor;
-    Cell();
-}
+    int neighbor;
+    bool isSearched;
+    Vector2 position;
+    int size;
+    Texture2D texture;
+    Cell(int x, int y, int size);
+    void draw();
+    void drawTexture();
+};
